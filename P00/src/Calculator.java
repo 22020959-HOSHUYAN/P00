02b77b6759cf90222b2e36e75344a4caadaa2454
22020959 <22020959@10.175.5.129>
2023-05-29 14:46:27 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,23 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 22020959, May 29, 2023 12:33:07 PM
+ */
+
+public class Calculator {
+	public int add(int a,int b) {
+		return a +b;
+	}
+	public int subtract(int a,int b) {
+		return a-b;
+	}
+	public int multiple(int a, int b) {
+		return a*b;
+	}
+	public int divide(int a, int b) {
+		return a/b;
+	}
+}
